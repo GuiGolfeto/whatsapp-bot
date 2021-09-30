005315f3f2d35164df5a9695f274ae099b790dbc
GuiGolfeto <ggfguigolfeto@gmail.com>
2021-09-30 11:45:55 -0300

    first commit

---
+++ whatsapp-bot-venv/bot.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'citação' in incoming_msg or 'frase' in incoming_msg: 
+        # retorne uma citação 
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'Não consegui recuperar uma citação neste momento, desculpe.'
+        msg.body(quote)
+        responded = True
+
+
+    if 'gato' in incoming_msg or 'gata' in incoming_msg:
+        # retorne uma foto de gato
+        msg.media('https://cataas.com/cat')
+        responded = True
+
+    if 'dog' in incoming_msg or 'cachorro' in incoming_msg:
+        # retorne uma foto de cachorro
+        msg.media('https://place.dog/300/200')
+        responded = True
+
+    if not responded:
+        msg.body('Só conheço frases e gatos famosos, desculpe!')
+    return str(resp)
+
+if __name__ == '__main__':
+   app.run()
